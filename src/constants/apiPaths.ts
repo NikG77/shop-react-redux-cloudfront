9e962d64f0e5309e81ca9e16ab29999c8e6ec63f
Georgy Nikitin <georgy_nikitin@epam.com>
2021-08-18 13:02:33 +0300

    Fixed import path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://v36qxqri4i.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://v36qxqri4i.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://v36qxqri4i.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://rids5x199f.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://v36qxqri4i.execute-api.eu-west-1.amazonaws.com/dev',
   cart: 'https://v36qxqri4i.execute-api.eu-west-1.amazonaws.com/dev',
 };
